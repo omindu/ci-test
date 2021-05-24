f4d6d1c57e74e24a98ebebe90caac45473b97a3b
omindu <omindu@wso2.com>
2021-05-24 19:03:29 +0530

    Add new files

---
+++ src/test/java/org/test/ci/App1Test.java
@@ -0,0 +1,21 @@
+package org.test.ci;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class App1Test
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("=======Step 3==========");
+        assertTrue( true );
+    }
+}
